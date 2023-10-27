fb3593384e748feb2b856983c9b9e25e0d99ae8f
Greger Stolt Nilsen <gregersn@gmail.com>
2023-10-27 20:32:50 +0200

    More frontend work

--- frontend/src/initiative_tracker.tsx
+++ frontend/src/initiative_tracker.tsx
@@ -6,16 +6,22 @@ import { Combatants } from './combatants';
 import { InitiativeList } from "./initiativelist";
 
 const combatantList = ["Alven", "Ankan", "Draken", "Dvärgen", "Mannen", "Vargen"];
+const websocket = new WebSocket("ws://localhost:8765/");
+
+function new_battle() {
+    console.log(window.location.search);
+    window.location.search = "page=GM";
+}
 
 const Menu = ()  => {
     return <div className="menu">
             <input id="gameIdInput" />
             <input id="gameSecretInput" /><button onClick={() => {console.log("Join");}} className="btn add">Join</button>
-            <button onClick={() => {console.log("New");}} className="btn add">New</button>
+            <button onClick={new_battle} className="btn add">New</button>
     </div>;
 };
 
-const GM = () => {
+const GM = (game: string|null, secret: string|null) => {
 return (<div className="row align-items-start">
         <div className="col">
             <InitiativeList combatants={combatantList} shift={true} />
@@ -23,6 +29,10 @@ return (<div className="row align-items-start">
         <div className="col">
             <Combatants combatants={combatantList} />
         </div></div>);
+}
+
+const Player = (game: string|null) => {
+    return (<div>Show the order, and list of combatants?</div>);
 }
 
 enum AppStates {
@@ -34,10 +44,18 @@ enum AppStates {
 const App = (props: {}) => { 
     const queryParams = new URLSearchParams(window.location.search);
     const state = queryParams.get("page");
+    const battle = queryParams.get("battle");
+    const secret = queryParams.get("secret");
     console.log(state);
 
     let content = <div></div>;
     switch(state) {
+        case "GM":
+            content = GM(battle, secret);
+            break;
+        case "player":
+            content = Player(battle);
+            break;
         case "menu":
         default:
             content = Menu();
