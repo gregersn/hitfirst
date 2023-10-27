760e30af28f1a276f3c2d1381796a0a4c90eaca2
Greger Stolt Nilsen <greger@fusetools.com>
2023-10-27 08:54:56 +0200

    Update frontend

--- frontend/src/initiative_tracker.tsx
+++ frontend/src/initiative_tracker.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import * as ReactDOM from 'react-dom'
+//import * as ReactDOM from 'react-dom'
 import * as ReactDOMClient from 'react-dom/client';
 
 import { Combatants } from './combatants';
@@ -7,17 +7,47 @@ import { InitiativeList } from "./initiativelist";
 
 const combatantList = ["Alven", "Ankan", "Draken", "Dvärgen", "Mannen", "Vargen"];
 
-const App = () => <div className="container text-center">
-    <div className="row align-items-start">
+const Menu = ()  => {
+    return <div className="menu">
+            <input id="gameIdInput" />
+            <input id="gameSecretInput" /><button onClick={() => {console.log("Join");}} className="btn add">Join</button>
+            <button onClick={() => {console.log("New");}} className="btn add">New</button>
+    </div>;
+};
+
+const GM = () => {
+return (<div className="row align-items-start">
         <div className="col">
             <InitiativeList combatants={combatantList} shift={true} />
         </div>
         <div className="col">
             <Combatants combatants={combatantList} />
-        </div>
-    </div>
-</div>;
+        </div></div>);
+}
+
+enum AppStates {
+    Menu = 1,
+    Player,
+    GM
+}
+
+const App = (props: {}) => { 
+    const queryParams = new URLSearchParams(window.location.search);
+    const state = queryParams.get("page");
+    console.log(state);
+
+    let content = <div></div>;
+    switch(state) {
+        case "menu":
+        default:
+            content = Menu();
+    }
+    
+    return (<div className="container text-center">
+        { content }
+        </div>)
+};
 
 const domContainer = document.getElementById("initiativeTracker");
 const root = ReactDOMClient.createRoot(domContainer!);
-root.render(App());
+root.render(App({}));
